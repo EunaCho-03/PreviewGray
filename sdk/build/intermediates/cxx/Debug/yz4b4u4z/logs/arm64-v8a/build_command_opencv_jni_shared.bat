30b69944cf135e9995e65996c467041bbb9b3926
EunaCho-03 <karen.cho0827@gmail.com>
2023-06-09 19:11:07 +0900

    Preview Gray

---
+++ sdk/build/intermediates/cxx/Debug/yz4b4u4z/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\QR22003\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\QR22003\\AndroidStudioProjects\\OpenCvCpp\\sdk\\.cxx\\Debug\\yz4b4u4z\\arm64-v8a" ^
+  opencv_jni_shared
